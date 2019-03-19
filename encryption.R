dac6d07454ce6aded19628a90bbd0673b5c617c1
pg-readify <paul.goodall@readify.net>
2019-03-19 17:52:54 +1100

    Initial Magical commit

---
+++ encryption.R
@@ -0,0 +1,17 @@
+
+
+source_encrypted <- function(my_url, my_fname, my_pwd_file, my_path=getwd()){
+  com <- "wget -q -O my_path/my_fname.enc my_url;
+  openssl aes-256-cbc -in my_path/my_fname.enc -d -pass file:my_pwd_file -out my_path/my_fname.R"
+  com <- gsub("my_url", my_url, com)
+  com <- gsub("my_path", my_path, com)  
+  com <- gsub("my_fname", my_fname, com)
+  com <- gsub("my_pwd_file", my_pwd_file, com)
+  system(com)
+  r_file <- paste0(my_path,"/", my_fname, ".R")
+  source(r_file)
+}
+
+pull_repo <- function(){
+  
+}
