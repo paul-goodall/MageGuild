a94c8b76e439bc380e7b223323e227050a9b2734
pg-readify <paul.goodall@readify.net>
2019-03-19 18:02:08 +1100

    another commit

--- encryption.R
+++ encryption.R
@@ -12,6 +12,18 @@ source_encrypted <- function(my_url, my_fname, my_pwd_file, my_path=getwd()){
   source(r_file)
 }
 
+git_push <- function(my_dir, my_comment="another commit"){
+  
+  com <- "git -C my_dir add -A;
+  git -C my_dir commit -m 'my_comment';
+  git -C my_dir push -u origin master;
+  "
+  com <- gsub("my_dir", my_dir, com)
+  com <- gsub("my_comment", my_comment, com)
+  cat(com)
+  system(com)
+}
+
 pull_repo <- function(){
   
 }
